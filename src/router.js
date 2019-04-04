3880995aee7dddbf40c70a1db649b5f944ca38fa
WWK563388548 <wc563388548@gmail.com>
2019-04-04 20:46:35 +0900

    set up style of component and it's route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import PortfolioOne from "./views/projectOne.vue";
+
 
 Vue.use(Router);
 
@@ -19,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/portfolio/one",
+      name: "PortfolioOne",
+      component: PortfolioOne
     }
   ]
 });
