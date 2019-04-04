9bd119413709b42ecbeff6ea848fdc14be4404a4
WWK563388548 <wc563388548@gmail.com>
2019-04-04 20:59:52 +0900

    add new component for projectTwo

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import PortfolioOne from "./views/projectOne.vue";
+import PortfolioTwo from "./views/projectTwo.vue";
 
 
 Vue.use(Router);
@@ -26,6 +27,11 @@ export default new Router({
       path: "/portfolio/one",
       name: "PortfolioOne",
       component: PortfolioOne
+    },
+    {
+      path: "/portfolio/two",
+      name: "PortfolioTwo",
+      component: PortfolioTwo
     }
   ]
 });
---
+++ src/views/projectTwo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="two main-content">
+    <div class="left-content slideUp">
+      <h1>Project Two</h1>
+
+      <router-link to='/portfolio/one'>
+        <img src="../assets/arrow-up.svg" class="arrow arrow-up" />
+      </router-link>
+
+      <p class="subtxt">XXXXXX</p>
+      <p class="subtxt">XXXXXX</p>
+
+      <router-link to='/portfolio/three'>
+        <img src="../assets/arrow-down.svg" class="arrow arrow-down" />
+      </router-link>
+    </div>
+    <div class="right-content img2">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PortfolioOne",
+  components: {
+    
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+    $page_color: #48b886;
+
+    h1 {
+        color: $page_color !important;
+    }
+
+    .img2 {
+        background: url("../assets/home.jpg");
+        background-size: cover;
+        background-position: 50%;
+
+        img {
+            widows: 50%;
+        }
+    }
+</style>
